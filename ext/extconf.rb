23c5cbe30be6055a1bad5473c7d1346be78dc9d0
AkitaOnRails <fabioakita@gmail.com>
2016-06-22 09:32:59 -0300

    adding missing crystal deps task, install task so bundle install doesn't complain, and llvm check in the extconf.rb file as suggested by @gaar4ica

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,7 @@
 require 'mkmf'
 
 abort 'missing crystal' unless find_executable('crystal')
+abort 'missing llvm tooling' unless find_executable('llvm-config')
 
 # Dirty patching
 def create_makefile(_, _ = nil)
